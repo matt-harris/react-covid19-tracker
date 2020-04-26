a9aa760b404cb7daf155dcb5aa8149fe57f4e2c2
Matt Harris <hello@matt-harris.net>
2020-04-26 12:31:24 +0100

    Refactored App to use Styled Components.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
 import CountUp from 'react-countup';
-import './Card.scss';
+import styled from 'styled-components';
 
-const Card = ({data, className, title, date, info}) => (
-  <div className={`Card ${className ? className : null}`}>
-    <div className="Card__title">{title}</div>
-    <div className="Card__count">
+const CardWrap = styled.div`
+  padding: 1rem;
+  border: 0.0625rem solid #ddd;
+  border-radius: 0.25rem;
+  box-shadow: 0 0.125rem 0.25rem 0px rgba(0, 0, 0, 0.2);
+  border-bottom: 0.25rem solid ${props => props.highlight || '#ddd'};
+`;
+
+const CardTitle = styled.div`
+  margin-bottom: 0.5rem;
+  font-weight: 700;
+  text-transform: uppercase;
+`;
+
+const CardCount = styled.div`
+  margin-bottom: 0.5rem;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+const CardDate = styled.div`
+  margin-bottom: 0.5rem;
+`;
+
+const CardInfo = styled.div`
+  font-weight: 700;
+`;
+
+
+const Card = ({data, title, date, info, highlight}) => (
+  <CardWrap highlight={highlight}>
+    <CardTitle>{title}</CardTitle>
+    <CardCount>
       <CountUp start={0} end={data.value} duration={2.5} separator="," />
-    </div>
-    <div className="Card__date">{date}</div>
-    <div className="Card__info">{info}</div>
-  </div>
+    </CardCount>
+    <CardDate>{date}</CardDate>
+    <CardInfo>{info}</CardInfo>
+  </CardWrap>
 )
 
 export default Card;
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import './Logo.scss';
+import styled from 'styled-components';
 
-const Logo = () => (
-  <>    
-    <h1 className="Logo">Covid-19 Tracker</h1>
-  </>
+const LogoHeading = styled.h1`
+  margin-bottom: 1rem;
+  font-size: 2rem;
+  font-weight: 900;
+
+  @media (min-width: 37.5rem) {
+    margin-right: 1rem;
+    margin-bottom: 0;
+  }
+
+  @media (min-width: 48rem) {
+    font-size: 3rem;
+  }
+`;
+
+const Logo = () => ( 
+  <LogoHeading>Covid-19 Tracker</LogoHeading>
 );
 
 export default Logo;
